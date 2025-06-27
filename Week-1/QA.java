618b5daee118d5615cb40102c151f8e756e02a4f
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-06-27 21:46:27 +0530

    Week-1 commit

---
+++ Week-1/QA.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+class QA{
+
+public static void main(String args[]){
+
+Scanner s = new Scanner(System.in);
+
+// 1 problem
+// System.out.println("Enter Your Name: ");
+// String name = s.nextLine();
+
+// System.out.println("Enter Your Age: ");
+// int age = s.nextInt();
+// s.nextLine();
+
+// System.out.println("Enter Your address: ");
+// String address = s.nextLine();
+
+
+
+// System.out.println("Name : " + name );
+// System.out.println("Age : " + age );
+// System.out.println("Address: " + address );
+
+// solved
+
+
+// 2 problem
+// System.out.println("Enter Your a: ");
+// int a = s.nextInt();
+
+// System.out.println("Enter Your b: ");
+// int b = s.nextInt();
+// s.nextLine();
+
+// System.out.println("Enter Your c: ");
+// int c = s.nextInt();
+
+// int d = a*b*c;
+
+// int e = a+b+c;
+
+// int res=d/e;
+
+// System.out.println("Result :" + res);
+
+
+//3 problem
+
+
+System.out.println("Enter Your name: ");
+String a= s.nextLine();
+
+System.out.println("Enter Your b: ");
+double b = s.nextInt();
+s.nextLine();
+
+System.out.println("Enter Your dept: ");
+String c = s.nextLine();
+
+
+System.out.println("Name : " + a );
+System.out.println("Score : " + b/10 + "/10" );
+System.out.println("Department: " + c );
+
+
+
+
+
+
+
+
+}
+
+
+
+}
---
+++ Week-1/adding_numbers.java
@@ -0,0 +1,17 @@
+class adding_numbers{
+    public static void main(String args[]){
+        // two-numbers
+        // int a=10;
+        // int b=20;
+
+        // System.out.println(a+b);
+
+        //three numbers
+        int a=10;
+        int b=20;
+        int c=30;
+        int d=a+b+c;
+        System.out.println("The sum : " + d);
+
+    }
+}
