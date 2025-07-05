a136e86d1c18f720abfa25bb52a75d2ec5dbcfa5
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-07-05 11:47:39 +0530

    Day-04 completed(03.05.59)

---
+++ Day-04/QA.java
@@ -0,0 +1,65 @@
+
+import java.util.Scanner;
+
+
+
+public class QA {
+    
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        
+        
+        //problem-1
+        // System.out.println("Number 1 :");
+        // int a=s.nextInt();
+        // System.out.println("Number 2 :");
+        // int b=s.nextInt();
+        
+        // for(int i = a ; i <= b ;i++){
+        //     System.out.println(i);
+        // }
+
+
+        // Problem-2
+        // for (int i = 1; i <=10; i++) {
+            
+        //     // print only even number
+
+        //     if (i%2==0) {
+        //         System.out.println(i);
+        //     }
+
+        // }
+
+
+        // problem-3
+        // int count = 0;
+        // for (int i=1; i<=10;i++){
+        //     if (i%2!=0) {
+        //         count++;
+        //         System.out.println("Odd Number : "+i);
+        //     }
+        // }
+        // System.out.println("Count of Odd number : " + count);   
+        
+
+
+        // problem-4
+
+        // for (int i = 1; i <=100; i++) {
+            
+        //     if (i%3==0 && i%5==0) {
+        //         System.out.println(i);
+        //     }
+
+        // }
+
+
+        
+
+    }
+
+
+}
---
+++ Day-04/forLoop.java
@@ -0,0 +1,12 @@
+public class forLoop {
+    
+
+    public static void main(String[] args) {
+        
+        for (int i = 5; i>0 ; i--) {
+        System.out.println(i);
+        }
+
+    }
+
+}
