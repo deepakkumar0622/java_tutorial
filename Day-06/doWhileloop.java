3b8d44c8cac59ce517e1a0250cda88348295577d
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-07-12 19:31:06 +0530

    Day-6 completed

---
+++ Day-06/doWhileloop.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public  class doWhileloop{
+    public static void main(String[] args) {
+
+        // int count=0;
+
+
+        Scanner scan = new Scanner(System.in);
+
+        // int a=0;
+        
+        // do { 
+        //     System.out.println("Enter a Number : ");
+        //     a = scan.nextInt();
+        // } while (a<10);
+    }
+}
