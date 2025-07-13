1adf53189b71a309298178ef08dcd5be53143b25
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-07-13 17:35:31 +0530

    Classes and objects completed

---
+++ Day-07/Hotel.java
@@ -0,0 +1,14 @@
+
+public class Hotel {
+    
+
+    int tea=10;
+    int coffee=20;
+
+    public static void main(String[] args) {
+
+        Hotel server = new Hotel();
+        System.out.println(server.tea);
+        System.out.println(server.coffee);
+    }
+}
---
+++ Day-07/Laptop.java
@@ -0,0 +1,30 @@
+
+public class Laptop {
+
+    String name ="";
+    String proc=" ";
+    int ram=0;
+    int price =0;
+    
+
+    public static void main(String[] args) {
+        
+        Laptop lap1 = new Laptop();
+
+        lap1.name="Hp";
+        lap1.proc="intel i5";
+        lap1.ram=16;
+        lap1.price=10000;
+
+        Laptop lap2 = new Laptop();
+
+        lap2.name="DELL";
+        lap2.proc="intel i5";
+        lap2.ram=32;
+        lap2.price=80000;
+
+
+
+    }
+
+}
