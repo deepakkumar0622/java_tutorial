e8379ae25e1ef0c926d356eed8105cbb8bf2382b
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-07-17 07:48:00 +0530

    For each completed

---
+++ Day-09/ForEach.java
@@ -0,0 +1,16 @@
+
+public class ForEach {
+
+    public static void main(String[] args) {
+        
+        int[] num={10,20,30,40,50};
+
+        for(int var:num){
+            System.out.println(var);
+        }
+
+
+    }
+    
+
+}
