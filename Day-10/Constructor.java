040df7bc294db65bc44a62da4d8119b7f31dd8d2
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-07-19 23:54:38 +0530

    Constructor completed

---
+++ Day-10/Constructor.java
@@ -0,0 +1,26 @@
+public class Constructor{
+    
+    int marks;
+    String name;
+
+    Constructor() {
+        System.out.println("Hello");
+    }
+
+    
+        
+    public static void main(String[] args) {
+        
+        Constructor obj = new Constructor();
+
+        
+
+        // System.out.println(obj.name);
+
+        
+
+
+
+
+    }
+}
