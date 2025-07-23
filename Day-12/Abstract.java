564fa321ffa6b9b5229ff84a6ec8af18a3d24b5b
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-07-23 21:44:19 +0530

    Abstraction completed

---
+++ Day-12/Abstract.java
@@ -0,0 +1,18 @@
+abstract class Vehicle1{
+    abstract void speed();
+}
+
+class Bike1 extends Vehicle1{
+    @Override
+    void speed() {
+        System.out.println("25 km/hr");
+    }
+}
+
+
+
+
+public class Abstract {
+
+
+}
