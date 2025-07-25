8d7a25c0067fc533524577911fb0017040a33aad
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-07-25 17:49:13 +0530

    Final keyword completed

---
+++ Day-13/Final.java
@@ -0,0 +1,28 @@
+
+final class A {
+
+    final void disp() {
+        System.out.println("Class A");
+    }
+}
+
+//final class cant have inheritance
+//class B extends A {
+//    //final method cant be overridden
+////    void disp(){
+////        System.out.println("Class B");
+////    }
+//}
+
+
+
+public class Final {
+
+    public static void main(String[] args) {
+//
+//        B obj = new B();
+//        obj.disp();
+
+    }
+
+}
