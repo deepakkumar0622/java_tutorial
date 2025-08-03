37629e17189eb2db66489919b5322c42e461c70b
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-08-03 11:40:12 +0530

    Lambda Completed

--- Day-14/Lambda.java
+++ Day-14/Lambda.java
@@ -4,8 +4,22 @@ interface A{
 }
 
 
+
+
+
 public class Lambda {
     public static void main (String[] args ){
+
+        // Functional Interface
+        // A obj = new A(){
+        //     public void disp(){
+        //         System.out.println("Display");
+        //     }
+        // };
+        // obj.disp();
+
+
+        // Alternative Lambda ()->
 
         A obj = ()-> System.out.println("Display");
         obj.disp();
