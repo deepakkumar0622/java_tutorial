2f3f4861d9463648b8be3d2182f7c4467a6aaa0a
Deepak Kumar R <96228787+deepakkumar0622@users.noreply.github.com>
2025-08-06 10:54:45 +0530

    File reading completed

---
+++ Day-16/Read.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class Read {
+
+    public static void main(String[] args) {
+
+
+        try {
+            FileWriter file = new FileWriter("output.txt", true);
+            BufferedWriter bw = new BufferedWriter(file);
+            bw.write("Hey Man");
+            bw.newLine();
+            bw.write("Okay");
+            bw.close();
+            System.out.println("Success");
+        } catch (Exception e) {
+            System.out.println("Failed");
+        }
+    }
+}
